d8c07d858030c610b068138d9389de0502099dcb
sabelogumede <iamsabelogumede@gmail.com>
2021-04-20 13:16:57 +0200

    created routes for dashboard and about with default to dash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dah';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  { path:"dashboard",component: DashboardComponent;}
+  { path:"dashboard",component: DashboardComponent },
+  { path:"about",component: AboutComponent },
+  { path:"",redirectTo: "dashboard", pathMatch: "full" },
 ];
 
 @NgModule({
