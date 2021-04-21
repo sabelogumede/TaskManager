4ce70ff176936234d830cdeac40f4465c75ed4fa
sabelogumede <iamsabelogumede@gmail.com>
2021-04-21 10:35:14 +0200

    Created dashboard view and uploaded profile image in assets

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  Designation: string;
+  Username: string;
+  NoOfTeamMembers: number;
+  TotalCostOfAllProjects: number;
+  PendingTasks: number;
+  UpComingProjects: number;
+  ProjectCost: number;
+  CurrentExpenditure: number;
+  AvailableFunds: number;
 
-  ngOnInit(): void {
+  constructor() {
+    this.Designation= "";
+    this.Username = "";
+    this.NoOfTeamMembers =0;
+    this.TotalCostOfAllProjects =0
+    this.PendingTasks =0;
+    this.UpComingProjects = 0;
+    this.ProjectCost = 0;
+    this.CurrentExpenditure =0;
+    this.AvailableFunds =0;
+  }
+
+  ngOnInit() {
+    this.Designation= "Team Leader";
+    this.Username = "Sabelo Gumede";
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240
+    this.PendingTasks = 15;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
   }
 
 }
