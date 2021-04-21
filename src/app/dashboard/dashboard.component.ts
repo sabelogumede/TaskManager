38d73d71952a894cd2ccffd92cfbd37da159ccf5
sabelogumede <iamsabelogumede@gmail.com>
2021-04-21 12:08:39 +0200

    added NgFor looks to help load data dynamically

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -33,6 +33,60 @@ export class DashboardComponent implements OnInit {
     this.ProjectCost = 2113507;
     this.CurrentExpenditure = 96788;
     this.AvailableFunds = 52536;
+
+    this.Clients = [
+      "ABC Infotech Ltd.", "DEF Software Solutions.", "GHI Industries."
+    ]
+    this.Projects = [
+      "Project A", "Project B", "Project C", "Project D"
+    ]
+
+    for(var i = 2019; i>= 2010; i--){
+      this.Years.push(i);
+    }
+
+    this.TeamMembersSummary = [
+      { Region: "East", TeamMembersCount: 20, TemporarilyUnavailableMembers: 4},
+      { Region: "West", TeamMembersCount: 15, TemporarilyUnavailableMembers: 8},
+      { Region: "South", TeamMembersCount: 17, TemporarilyUnavailableMembers: 1},
+      { Region: "North", TeamMembersCount: 15, TemporarilyUnavailableMembers: 6},
+    ]
+
+    this.TeamMembers = [
+      {
+        Region:"East", Members: [
+          {ID: 1, Name: "Sabelo", Status: "Available"},
+          {ID: 1, Name: "Gogo", Status: "Available"},
+          {ID: 1, Name: "Sipho", Status: "Busy"},
+          {ID: 1, Name: "Thabile", Status: "Busy"},
+        ]
+      },
+      {
+        Region:"West", Members: [
+          {ID: 1, Name: "Sabelo", Status: "Available"},
+          {ID: 1, Name: "Gogo", Status: "Available"},
+          {ID: 1, Name: "Sipho", Status: "Busy"},
+          {ID: 1, Name: "Thabile", Status: "Busy"},
+        ]
+      },
+      {
+        Region:"South", Members: [
+          {ID: 1, Name: "Sabelo", Status: "Available"},
+          {ID: 1, Name: "Gogo", Status: "Available"},
+          {ID: 1, Name: "Sipho", Status: "Busy"},
+          {ID: 1, Name: "Thabile", Status: "Busy"},
+        ]
+      },
+      {
+        Region:"North", Members: [
+          {ID: 1, Name: "Sabelo", Status: "Available"},
+          {ID: 1, Name: "Gogo", Status: "Available"},
+          {ID: 1, Name: "Sipho", Status: "Busy"},
+          {ID: 1, Name: "Thabile", Status: "Busy"},
+        ]
+      }
+    ]
+
   }
 
 }
